cd089035e538f00e15767cd15c6d633ff2bcbb39
TsubasaImoto <tsubasa.imoto@gmail.com>
2022-06-10 09:37:15 +0900

    format

--- xunit.py
+++ xunit.py
@@ -4,7 +4,7 @@ class TestCase:
 
     def setUp(self):
         pass
-    
+
     def tearDown(self):
         pass
 
@@ -18,74 +18,79 @@ class TestCase:
             result.testFailed()
         self.tearDown()
 
+
 class TestResult:
     def __init__(self):
         self.runCount = 0
         self.errorCount = 0
-    
+
     def testStarted(self):
         self.runCount = self.runCount + 1
 
     def testFailed(self):
         self.errorCount = self.errorCount + 1
-   
+
     def summary(self):
         return f"{self.runCount} run, {self.errorCount} failed"
+
 
 class TestSuite:
     def __init__(self):
         self.tests = []
-    
+
     def add(self, test):
         self.tests.append(test)
-    
+
     def run(self, result):
         for test in self.tests:
             test.run(result)
 
+
 class WasRun(TestCase):
     def setUp(self):
         self.log = "setUp "
-    
+
     def testMethod(self):
         self.log = self.log + "testMethod "
-    
+
     def testBrokenMethod(self):
         raise Exception
 
     def tearDown(self):
         self.log = self.log + "tearDown "
 
+
 class TestCaseTest(TestCase):
     def setUp(self):
         self.result = TestResult()
-        
+
     def testTemplateMethod(self):
         test = WasRun("testMethod")
         test.run(self.result)
-        assert ("setUp testMethod tearDown " == test.log)
-    
+        assert "setUp testMethod tearDown " == test.log
+
     def testResult(self):
         test = WasRun("testMethod")
         test.run(self.result)
-        assert ("1 run, 0 failed" == self.result.summary())
+        assert "1 run, 0 failed" == self.result.summary()
 
     def testFailedResult(self):
         test = WasRun("testBrokenMethod")
         test.run(self.result)
-        assert ("1 run, 1 failed" == self.result.summary())
+        assert "1 run, 1 failed" == self.result.summary()
 
     def testFailedResultFormatting(self):
         self.result.testStarted()
         self.result.testFailed()
-        assert ("1 run, 1 failed" == self.result.summary())
-    
+        assert "1 run, 1 failed" == self.result.summary()
+
     def testSuite(self):
         suite = TestSuite()
         suite.add(WasRun("testMethod"))
         suite.add(WasRun("testBrokenMethod"))
         suite.run(self.result)
-        assert ("2 run, 1 failed" == self.result.summary())
+        assert "2 run, 1 failed" == self.result.summary()
+
 
 if __name__ == "__main__":
     suite = TestSuite()
